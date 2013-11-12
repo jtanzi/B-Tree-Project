ee5d7512be5867d2b0f6a6322c104b27d0e1d1cc
jtanzi <jtanzi@ku.edu>
2013-11-12 16:03:15 -0600

    Updated classes

---
+++ Disk.cpp
@@ -0,0 +1,43 @@
+/*
+Disk class functions definitions file
+*/
+
+#include <iostream>
+#include "Disk.h"
+
+using namespace std;
+
+//Construction
+/*
+Disk constructor
+*/
+/*Disk::Disk()
+{
+	
+}*/
+
+//Page management functions
+/*
+request_page function
+Arguments: pid (Page ID of the page to request)
+Returns a Page with the ID requested
+*/
+Page Disk::request_page(char* pid)
+{
+	Page new_Page = Page(pid);
+	update_occupied(pid);
+
+	return new_Page;
+}
+
+
+
+void Disk::update_occupied(char* pid)
+{
+	occupied.push(pid);
+	free.pop();
+}		
+		
+
+
+
